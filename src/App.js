81de0a823047426dc07a90d45bbdd3dceb57f01c
Marisa-Salazar <salazar.marisa15@gmail.com>
2023-11-15 22:18:54 -0700

    integrated basic html format

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -6,19 +7,20 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <footer>
-          This project was coded by Marisa Salazar and is open-sourced on
-          <a
-            className="App-link"
-            href="https://github.com/Marisa-Salazar/Weather-App-React"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            Github{" "}
-          </a>
-        </footer>
+        <Weather />
       </div>
+      <footer>
+        This project was coded by Marisa Salazar and is open-sourced on
+        <a
+          className="App-link"
+          href="https://github.com/Marisa-Salazar/Weather-App-React"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Github{" "}
+        </a>
+      </footer>
     </div>
   );
 }
