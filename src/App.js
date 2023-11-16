0e1076b6fe76069e3e3e04f5a85c8cbae69cb831
Marisa-Salazar <salazar.marisa15@gmail.com>
2023-11-15 21:43:04 -0700

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Marisa Salazar and is open-sourced on
-        <a
-          className="App-link"
-          href="https://github.com/Marisa-Salazar/Weather-App-React"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Github{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Marisa Salazar and is open-sourced on
+          <a
+            className="App-link"
+            href="https://github.com/Marisa-Salazar/Weather-App-React"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Github{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
