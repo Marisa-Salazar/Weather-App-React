1484f2d50f5b8ef1bae4bcb79e304cbc69227260
Marisa-Salazar <salazar.marisa15@gmail.com>
2023-11-16 20:23:37 -0700

    styled with css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <h1 className="text-center">Weather App</h1>
         <Weather />
       </div>
       <footer>
--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -29,7 +34,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-          60° F
+          <span className="temperature">60</span>
+          <span className="unit">° F</span>
         </div>
         <div className="col-6">
           <ul>
